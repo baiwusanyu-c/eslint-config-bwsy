986a375a16127e46f2ec2a6490d6805cb18b6a31
baiwusanyu-c <740132583@qq.com>
2023-12-18 15:36:03 +0800

    feat: added prettier config

---
+++ packages/prettier-config/index.js
@@ -0,0 +1,34 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  overrides: [
+    {
+      files: [
+        '**/node_modules/**',
+        '**/dist/**',
+        '**/output/**',
+        '**/coverage/**',
+        '**/temp/**',
+        '**/.vitepress/cache/**',
+        '**/.nuxt/**',
+        '**/.vercel/**',
+        '**/.changeset/**',
+        '**/.idea/**',
+        '**/.output/**',
+        '**/.vite-inspect/**',
+
+        '**/CHANGELOG*.md',
+        '**/*.min.*',
+        '**/LICENSE*',
+        '**/__snapshots__',
+        '**/auto-import?(s).d.ts',
+        '**/components.d.ts',
+        '**/pnpm-lock.yaml',
+      ],
+      options: {
+        requirePragma: true,
+      },
+    },
+  ],
+}
